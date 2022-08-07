110c6c0be6eb2fbcf2ce8958ba3280a42fcb29a4
uttamsaha <uttamsaha982@gmail.com>
2022-08-07 10:55:07 +0600

    updated banner

---
+++ js/app.js
@@ -0,0 +1,29 @@
+var swiper = new Swiper(".mySwiper", {
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      }
+});
+// var swiper = new Swiper(".mySwiper", {
+//     slidesPerView: 3,
+//     autoplay: {
+//       delay: 2500,
+//       disableOnInteraction: false,
+//     },
+//     spaceBetween: 50,
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//     },
+//     breakpoints: {
+//       0: {
+//         slidesPerView: 1,
+//       },
+//       520: {
+//         slidesPerView: 2,
+//       },
+//       950: {
+//         slidesPerView: 3,
+//       }
+//     }
+//   });
