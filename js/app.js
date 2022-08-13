b7a7132beff57ee88bd79caad046f60d99088ee2
uttamsaha <uttamsaha982@gmail.com>
2022-08-13 13:34:46 +0600

    responsive navbar added

--- js/app.js
+++ js/app.js
@@ -4,26 +4,10 @@ var swiper = new Swiper(".mySwiper", {
         disableOnInteraction: false,
       }
 });
-// var swiper = new Swiper(".mySwiper", {
-//     slidesPerView: 3,
-//     autoplay: {
-//       delay: 2500,
-//       disableOnInteraction: false,
-//     },
-//     spaceBetween: 50,
-//     pagination: {
-//       el: ".swiper-pagination",
-//       clickable: true,
-//     },
-//     breakpoints: {
-//       0: {
-//         slidesPerView: 1,
-//       },
-//       520: {
-//         slidesPerView: 2,
-//       },
-//       950: {
-//         slidesPerView: 3,
-//       }
-//     }
-//   });
+
+
+const faBars = document.querySelector('.fa-bars');
+const navItems = document.querySelector('.nav-items');
+faBars.addEventListener('click',()=>{
+  navItems.classList.toggle("active");
+})
